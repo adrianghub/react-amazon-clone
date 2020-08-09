d2359df585b1b5cb4590991b586d077f0d3024f6
Adrian <adrian_zinko@op.pl>
2020-08-09 22:02:34 +0200

    feat: setup navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import * as styles from "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header/Header'
+
 
 const App = () => {
   return (
@@ -8,13 +10,14 @@ const App = () => {
       <div className={styles.App}>
         <Switch>
             <Route path="/checkout">
-                <h1>Checkout</h1>
+                <h1>Checkout</h1>  
             </Route>
             <Route path="/login">
                 <h1>Login</h1>
             </Route>
             {/* default route */}
             <Route path="/">
+                <Header />
                 <h1>Home Page</h1>
             </Route>
         </Switch>
